37a228087ed5ca87dffb25e73438675cf80e36e6
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-09-18 08:39:40 +0900

    feat: env test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Next App</div>;
+  return (
+    <div>
+      Next App {process.env.NODE_ENV} {process.env.MESSAGE}
+    </div>
+  );
 };
 
 export default Home;
